7df70903cf7e274912b6b84e3aa48455d06ef74b
Nick Mars <niclaus.a@gmail.com>
2020-10-16 07:45:01 -0400

    data set use in applicaiton

---
+++ src/data/index.js
@@ -0,0 +1,43 @@
+export const purchases = [
+    {
+        name: "Nick",
+        time: new Date("9/10/2020 5:20:60"),
+        spent:75
+    },
+    {
+        name: "Nick",
+        time: new Date("9/29/2020 7:20:60"),
+        spent:120
+    },
+    {
+        name: "Nick",
+        time: new Date("10/01/2020 3:20:60"),
+        spent:200
+    },
+    {
+        name: "Nick",
+        time: new Date("10/06/2020 2:20:60"),
+        spent:95
+    },
+    {
+        name: "Nick",
+        time: new Date("10/13/2020 1:20:60"),
+        spent:320
+    },
+    {
+        name: "Katty",
+        time: new Date("10/13/2020 1:20:60"),
+        spent:320
+    },
+    {
+        name: "Chamber",
+        time: new Date("10/13/2020 1:20:60"),
+        spent:200
+    },
+    {
+        name: "Kem",
+        time: new Date("10/13/2020 1:20:60"),
+        spent:80
+    }
+
+];
