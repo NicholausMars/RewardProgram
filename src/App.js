e71ff399f840716e84238c18d53300eeb5d89b14
Nick Mars <niclaus.a@gmail.com>
2020-10-16 07:46:27 -0400

    default place in application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Container from "./component/container"
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Rewards program </h1>
+      <Container/>
     </div>
   );
 }
 
 export default App;
+
+
+/**
+ * 
+ * Interview coding ASSIGNMENT--for these roles, make it a REACT based solution
+
+A retailer offers a rewards program to its customers, awarding points based on each recorded purchase.
+
+A customer receives 2 points for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent over $50 in each transaction
+
+(e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
+
+Given a record of every transaction during a three month period, calculate the reward points earned for each customer per month and total.
+
+    Make up a data set to best demonstrate your solution
+    Check solution into GitHub
+ */
